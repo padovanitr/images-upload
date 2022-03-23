f71f5583d124281614502c702a541249a0e681b6
Thiago Padovani <padovanitr@gmail.com>
2022-03-22 22:26:35 -0300

    feat: getting data from fauna, format data and handling loading and error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,32 @@ import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
+interface Image {
+  title: string;
+  description: string;
+  url: string;
+  ts: number;
+  id: string;
+}
+
+type GetImagesResponse = {
+  after: string;
+  data: Image[];
+};
+
 export default function Home(): JSX.Element {
+  async function fetchImages({ pageParam = null }): Promise<GetImagesResponse> {
+    const { data } = await api.get('/api/images', {
+      params: {
+        after: pageParam
+      },
+    });
+
+    console.log('data', data)
+
+    return data;
+  }
+
   const {
     data,
     isLoading,
@@ -16,20 +41,21 @@ export default function Home(): JSX.Element {
     isFetchingNextPage,
     fetchNextPage,
     hasNextPage,
-  } = useInfiniteQuery(
-    'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    ,
-    // TODO GET AND RETURN NEXT PAGE PARAM
-  );
+  } = useInfiniteQuery('images', fetchImages, {
+    getNextPageParam: (lastPage, pages) => lastPage.after || null,
+  });
 
   const formattedData = useMemo(() => {
-    // TODO FORMAT AND FLAT DATA ARRAY
+    return data?.pages.flatMap(pageData => pageData.data.flat());
   }, [data]);
 
-  // TODO RENDER LOADING SCREEN
+  if (isLoading) {
+    return <Loading />
+  }
 
-  // TODO RENDER ERROR SCREEN
+  if (isError) {
+    return <Error />
+  }
 
   return (
     <>
